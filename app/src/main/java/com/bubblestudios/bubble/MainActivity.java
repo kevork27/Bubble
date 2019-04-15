74d67f54da3860e8211ff8506b6ef663f99bd38c
minazanna <zannamin@bu.edu>
2019-04-15 13:07:11 -0400

    Artist profile page First page Added image to userprof.xml

--- app/src/main/java/com/bubblestudios/bubble/MainActivity.java
+++ app/src/main/java/com/bubblestudios/bubble/MainActivity.java
@@ -3,17 +3,23 @@ package com.bubblestudios.bubble;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements CardsFragment.OnFragmentInteractionListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     @Override
     public void onFragmentInteraction(Uri uri) {
+
+
 
     }
 }
